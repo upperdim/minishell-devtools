6050c00945bc4e258b038976f7c455eac51c7df2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-07-08 20:29:27 +0200

    add token types to test cases

--- parser_prot.py
+++ parser_prot.py
@@ -172,30 +172,31 @@ def tests():
 	tests = [
 		# Split from redirecitons
 		['echo asd>hello', 
-   			['echo',
-	   		'asd',
-			'>',
-			'hello'
+   			[['echo', TokenType.STRING],
+	   		['asd', TokenType.STRING],
+			['>', TokenType.APPEND_TO],
+			['hello', TokenType.STRING]
 			]
 		],
 		['echo asd<hello',
-   			['echo',
-	   		'asd',
-			'<',
-			'hello'
+   			[['echo', TokenType.STRING],
+	   		['asd', TokenType.STRING],
+			['<', TokenType.REDIR_FROM],
+			['hello', TokenType.STRING]
 			]
 		],
 		['echo asd>>hello',
-   			['echo',
-	   		'asd',
-			'>>',
-			'hello']
+   			[['echo', TokenType.STRING],
+	   		['asd', TokenType.STRING],
+			['>>', TokenType.APPEND_TO],
+			['hello', TokenType.STRING]
+			]
 		],
 		['echo asd<<hello',
-   			['echo',
-	   		'asd',
-			'<<',
-			'hello'
+   			[['echo', TokenType.STRING],
+	   		['asd', TokenType.STRING],
+			['<<', TokenType.HERE_DOC],
+			['hello', TokenType.STRING],
 			]
 		],
 
@@ -205,108 +206,108 @@ def tests():
 		# IDEA 1: make an exception list of patterns that will be ignored by the rule that separates this quote
 		# IDEA 2: just do another pass looking for redirection patterns and quotes. Can be done even in token linked list
 		['cat <<" EOF"',
-   			['cat',
-	   		'<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat <" EOF"',
-   			['cat',
-			'<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<', TokenType.REDIR_FROM],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat >>" EOF"',
-   			['cat',
-	   		'>>',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['>>', TokenType.APPEND_TO],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat >" EOF"',
-   			['cat',
-	   		'>',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['>', TokenType.REDIR_TO],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat <<\' EOF\'',
-   			['cat',
-			'<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat <\' EOF\'',
-   			['cat',
-			'<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<', TokenType.REDIR_FROM],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat >>\' EOF\'',
-   			['cat',
-	   		'>>',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['>>', TokenType.APPEND_TO],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat >\' EOF\'',
-   			['cat',
-	   		'>',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['>', TokenType.REDIR_TO],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		
 		# Redirections w/ prefix & fd
 		['cat hello<<" EOF"',
-   			['cat',
-	   		'hello',
-			'<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['hello', TokenType.STRING],
+			['<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat 123<<" EOF"',
-   			['cat',
-	   		'123<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+			['123<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		],
 		['cat hello123<<" EOF"',
-   			['cat',
-	   		'hello123',
-			'<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+			['hello123', TokenType.STRING],
+			['<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		], # has to be entirely digits like above in order to get detected as a file descriptor for the redirection
 		['cat hello1>file2name 123>file1name123>world<<" EOF"',
-   			['cat',
-			'hello1',
-			'>',
-			'file2name',
-			'123>',
-			'file1name123',
-			'>',
-			'world',
-			'<<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+			['hello1', TokenType.STRING],
+			['>', TokenType.REDIR_TO],
+			['file2name', TokenType.STRING],
+			['123>', TokenType.REDIR_TO],
+			['file1name123', TokenType.STRING],
+			['>', TokenType.REDIR_TO],
+			['world', TokenType.STRING],
+			['<<', TokenType.HERE_DOC],
+			[' EOF', TokenType.STRING],
 			]
 		],
 
 		# Redirections w/ prefix & fd: RANGE CHECK
 		['cat -12> filename',
-   			['cat',
-	   		'-12',
-			'>',
-			'filename'
+   			[['cat', TokenType.STRING],
+	   		['-12', TokenType.STRING],
+			['>', TokenType.REDIR_TO],
+			['filename', TokenType.STRING],
 			]
 		], # since - is not digit, negative numbers count as filenames
 		['cat 2147483647> filename',
-   			['cat',
-	   		'2147483647>',
-			'filename'
+   			[['cat', TokenType.STRING],
+	   		['2147483647>', TokenType.REDIR_TO],
+			['filename', TokenType.STRING],
 			]
 		],
 		['cat 2147483648> filename',
-   			['cat',
-	   		'2147483648',
-			'>',
-			'filename'
+   			[['cat', TokenType.STRING],
+	   		['2147483648', TokenType.STRING],
+			['>', TokenType.REDIR_TO],
+			['filename', TokenType.STRING],
 			]
 		],
 
@@ -315,19 +316,19 @@ def tests():
 		# These will be labeled as SYNTAX ERRORs either after tokenization or maybe here in lexing process
 		# If you have a reliable way of cutting them out, just stop processing and give error to user
 		['cat << >> <" EOF"',
-   			['cat',
-	   		'<<',
-			'>>',
-			'<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<<', TokenType.HERE_DOC],
+			['>>', TokenType.APPEND_TO],
+			['<', TokenType.REDIR_FROM],
+			[' EOF', TokenType.STRING],
 			]
 		], # expected is discussible
 		['cat <<>> <" EOF"',
-   			['cat',
-	   		'<<',
-			'>>',
-			'<',
-			' EOF'
+   			[['cat', TokenType.STRING],
+	   		['<<', TokenType.HERE_DOC],
+			['>>', TokenType.APPEND_TO],
+			['<', TokenType.REDIR_FROM],
+			[' EOF', TokenType.STRING],
 			]
 		],  # expected is discussible
 
@@ -336,68 +337,120 @@ def tests():
 		
 		# Quotes & Spaces
 		['export VAR="echo hi | cat"',
-   			['export',
-	   		'VAR=echo hi | cat'
+			[['export', TokenType.STRING],
+			['VAR=echo hi | cat', TokenType.STRING],
 			]
 		],
 		['echo "Hello"World',
-   			['echo',
-	   		'HelloWorld'
+   			[['echo', TokenType.STRING],
+			['HelloWorld', TokenType.STRING],
 			]
 		],
 		['echo Hello World',
-   			['echo',
-	   		'Hello',
-			'World'
+   			[['echo', TokenType.STRING],
+	   		['Hello', TokenType.STRING],
+			['World', TokenType.STRING],
 			]
 		],
 		['echo "Hello  World"',
-   			['echo',
-	   		'Hello  World'
+   			[['echo', TokenType.STRING],
+	   		['echo', TokenType.STRING],
 			]
 		],
 		#['echo "Hello\' World"', ['echo', "Hello' World"]],
-		['echo "Hello" World"',
-   			['echo',
-	   		'Hello',
-			'World"'
-			]
-		],
+		#['echo "Hello" World"', ['echo', 'Hello', 'World"']],
 		#['echo Hello" World', ['echo', 'Hello"', 'World']],
 		['echo Hello"World"       ',
-   			['echo',
-	   		'HelloWorld'
+   			[['echo', TokenType.STRING],
+	   		['HelloWorld', TokenType.STRING],
 			]
 		],
 		['echo              Hello"World"\'stuck\'        ',
-   			['echo',
-	   		'HelloWorldstuck'
+   			[['echo', TokenType.STRING],
+	   		['HelloWorldstuck', TokenType.STRING],
 			]
 		],
 		['ec ho"  \'Hello  "World\'  x ',
-   			['ec',
-	   		"ho  'Hello  World'",
-			'x'
+   			[['ec', TokenType.STRING],
+	   		["ho  'Hello  World'", TokenType.STRING],
+			['x', TokenType.STRING],
 			]
 		],
-		['"\'"\'\'"\'"', ["''"]],
-		['a"\'123\'456"', ["a'123'456"]],
-		['"\'123\'456"', ["'123'456"]],
-		['"\'"', ["'"]],
-		["''", ['']],
-		['""', ['']],
-		['\'"\'', ['"']],
-		['\'\'"\'"', ["'"]],
-		['echo" Hello World"', ['echo Hello World']],
-		['"no clue of \'\'what other test   "to do\'', ["no clue of ''what other test   to", "do'"]],
-		['" dog" cat', [' dog', 'cat']],
+		['"\'"\'\'"\'"',
+   			[["''", TokenType.STRING]]
+		],
+		['a"\'123\'456"',
+   			[["a'123'456", TokenType.STRING]]
+		],
+		['"\'123\'456"',
+   			[["'123'456", TokenType.STRING]]
+		],
+		['"\'"',
+   			[["'", TokenType.STRING]]
+		],
+		["''",
+   			[['', TokenType.STRING]]
+		],
+		['""',
+   			[['', TokenType.STRING]]
+		],
+		['\'"\'',
+   			[['"', TokenType.STRING]]
+		],
+		['\'\'"\'"',
+   			[["'", TokenType.STRING]]
+		],
+		['echo" Hello World"',
+   			[["echo Hello World", TokenType.STRING]]
+		],
+		['"no clue of \'\'what other test   "to do',
+   			[["no clue of ''what other test   to", TokenType.STRING],
+	   		["do", TokenType.STRING],
+			]
+		],
+		['" dog" cat',
+   			[[" dog", TokenType.STRING],
+	   		["cat", TokenType.STRING],
+			]
+		],
 
 		# Pipes
-		['echo hi | cat -e', ['echo', 'hi', '|', 'cat', '-e']],
-		['echo hi|cat"asd" -e', ['echo', 'hi', '|', 'catasd', '-e']],
-		['echo hi|cat -e', ['echo', 'hi', '|', 'cat', '-e']],
-		['echo "hi|cat" -e', ['echo', 'hi|cat', '-e']],
-		['echo hi"|"cat -e', ['echo', 'hi|cat', '-e']],
+		['echo hi | cat -e',
+   			[["echo", TokenType.STRING],
+	   		["hi", TokenType.STRING],
+			["|", TokenType.PIPE],
+			["cat", TokenType.STRING],
+			["-e", TokenType.STRING],
+			]
+		],
+		['echo hi|cat -e',
+   			[["echo", TokenType.STRING],
+	   		["hi", TokenType.STRING],
+			["|", TokenType.PIPE],
+			["cat", TokenType.STRING],
+			["-e", TokenType.STRING],
+			]
+		],
+		['echo hi|cat"asd" -e',
+   			[["echo", TokenType.STRING],
+	   		["hi", TokenType.STRING],
+			["|", TokenType.PIPE],
+			["catasd", TokenType.STRING],
+			["-e", TokenType.STRING],
+			]
+		],
+		['echo "hi|cat" -e',
+   			[["echo", TokenType.STRING],
+	   		["hi|cat", TokenType.STRING],
+			["-e", TokenType.STRING],
+			]
+		],
+		['echo hi"|"cat -e',
+   			[["echo", TokenType.STRING],
+	   		["hi|cat", TokenType.STRING],
+			["-e", TokenType.STRING],
+			]
+		],
 	]
 
 
