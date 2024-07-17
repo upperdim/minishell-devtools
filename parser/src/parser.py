78b616d3a6ada61bb9be207a21579e95fc71bb2d
upperdim <45048352+upperdim@users.noreply.github.com>
2024-07-17 15:07:29 +0200

    removed unnecessary error log

--- parser/src/parser.py
+++ parser/src/parser.py
@@ -57,10 +57,6 @@ def interactive():
 	while True:
 		line = input('$ ')
 		tokens = parse(line)
-		if not check_token_rules(tokens):
-			print('=====================')
-			print('ERROR! INVALID TOKENS')
-			print('=====================')
 		print_ll(tokens)
 
 
